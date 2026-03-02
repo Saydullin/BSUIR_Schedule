c51c30c9e0f3868fb753ba689e96fff4331ae207
Saydullin <ssaydalihudjaev@devsgroup.net>
2026-03-02 16:47:32 +0300

    agp upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("androidx.room") version "2.8.4" apply false
 }
